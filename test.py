3971b0a59ee5aca009e57b5d40d89dbd7d6309d4
José Aguilar <jc.aguilar1308@gmail.com>
2025-01-23 19:30:27 -0600

    feat: :wastebasket: Se logró la ejecución del emulador.

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 def main():
     env = gym_super_mario_bros.make('SuperMarioBros-v0')
     env = JoypadSpace(env, SIMPLE_MOVEMENT)
-    done = True
+    terminated = True
 
     for step in range(5000):
-        if done:
+        if terminated:
             state = env.reset()
-        state, reward, done, info = env.step(env.action_space.sample())
+        observation, reward, terminated, info = env.step(env.action_space.sample())
         env.render()
 
     env.close()
