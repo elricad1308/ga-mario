10d3f382e32b124b44ce15ffbc5be252a8eb9e27
José Aguilar <jc.aguilar1308@gmail.com>
2025-01-23 18:12:22 -0600

    feat: :building_construction: Prueba con versión actualizada de Gym y SMB

---
+++ test.py
@@ -0,0 +1,20 @@
+import gym
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+def main():
+    env = gym_super_mario_bros.make('SuperMarioBros-v0')
+    env = JoypadSpace(env, SIMPLE_MOVEMENT)
+    done = True
+
+    for step in range(5000):
+        if done:
+            state = env.reset()
+        state, reward, done, info = env.step(env.action_space.sample())
+        env.render()
+
+    env.close()
+
+if __name__ == "__main__":
+    main()
